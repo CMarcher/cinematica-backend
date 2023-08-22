8892198a27644fa8572a339cd206673bc511160e
Caleb Archer <calebandcorey@gmail.com>
2023-08-22 16:47:18 +1200

    Fix indentation

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -1,8 +1,8 @@
 terraform {
     required_providers {
-		aws = {
-			source = "hashicorp/aws"
-			version = "5.13.1"
-		}
+        aws = {
+              source = "hashicorp/aws"
+              version = "5.13.1"
+        }
     }
 }
