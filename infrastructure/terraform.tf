b149cd74398fd7cb44fda7169e220b4ecaa0b6b3
Caleb Archer <calebandcorey@gmail.com>
2023-09-09 01:28:08 +1200

    Remove name from API Gateway Resource (#25)

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -118,7 +118,6 @@ resource "aws_api_gateway_domain_name" "cinematica_api_domain" {
 }
 
 resource "aws_api_gateway_resource" "cinematica_api_gateway_resource" {
-    name = "proxy"
     parent_id = aws_api_gateway_rest_api.cinematica_api_gateway.root_resource_id
     rest_api_id = aws_api_gateway_rest_api.cinematica_api_gateway.id
     path_part = "{proxy+}"
