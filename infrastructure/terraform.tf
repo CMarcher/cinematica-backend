c1c1c59ce4eee0ddbd8d00d143214b3da2a10e3c
Caleb Archer <calebandcorey@gmail.com>
2023-09-09 14:41:55 +1200

    Update source ARN for API Gateway Lambda permissions

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -163,7 +163,8 @@ resource "aws_lambda_permission" "api_gateway_lambda_permission" {
     action        = "lambda:InvokeFunction"
     function_name = aws_lambda_function.cinematica_api_lambda.function_name
     principal     = "apigateway.amazonaws.com"
-    source_arn = "arn:aws:execute-api:${var.region}:${local.account_id}:${aws_api_gateway_rest_api.cinematica_api_gateway.id}/*/${aws_api_gateway_method.cinematica_api_gateway_proxy_method.http_method}${aws_api_gateway_resource.cinematica_api_gateway_resource.path}"
+    source_arn = "arn:aws:execute-api:${data.aws_region.api_gateway_region.name}:${local.account_id}:${aws_api_gateway_rest_api.cinematica_api_gateway.id}/*/${aws_api_gateway_method.cinematica_api_gateway_proxy_method.http_method}${aws_api_gateway_resource.cinematica_api_gateway_resource.path}"
+}
 }
 
 # # # # #
