96893f7697b8cb813e147acffa70bf264549d2a7
Caleb Archer <calebandcorey@gmail.com>
2023-09-09 16:33:19 +1200

    Add missing provider specification which caused failure

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -139,6 +139,7 @@ resource "aws_api_gateway_domain_name" "cinematica_api_domain" {
 }
 
 resource "aws_api_gateway_base_path_mapping" "api_mapping" {
+    provider = aws.us_east
     api_id      = aws_api_gateway_rest_api.cinematica_api_gateway.id
     domain_name = aws_api_gateway_domain_name.cinematica_api_domain.domain_name
     stage_name = aws_api_gateway_stage.cinematica_production.stage_name
