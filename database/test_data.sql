041c998d5ed1f18ec00826dbd6e06a2ca6363dca
David <d.nguyen7381@gmail.com>
2023-09-11 16:01:01 +1200

    properly pushed test_data.sql

---
+++ database/test_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (user_id) VALUES 
+	-- adds jeffery
+	('a33c0775-1406-4cc3-81ec-16151ecc4ade'),
+	-- adds david
+	('93cfcbd6-54b6-4961-bec5-0cf6e0a81917'),
+	-- adds test
+	('e8f2aa40-b1a8-46f2-81c3-4e0dbc3e4f9d');
+
+
+select * from users;
