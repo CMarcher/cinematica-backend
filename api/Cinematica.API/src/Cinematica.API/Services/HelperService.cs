68e5fbbce8e665097a681521ffb7d43d6e1650f5
David <d.nguyen7381@gmail.com>
2023-09-16 13:47:14 +1200

    added auth to appropriate api calls in users, replies and posts controllers

--- api/Cinematica.API/src/Cinematica.API/Services/HelperService.cs
+++ api/Cinematica.API/src/Cinematica.API/Services/HelperService.cs
@@ -14,7 +14,7 @@ public interface IHelperService
     Task<string> DownloadFile(string url, string savePath);
     Task<UserType?> FindUserByEmailAddress(string emailAddress);
     Task<UserType?> GetCognitoUser(string id);
-    Task<bool> CheckTokenSub(string tokenString, string userId);
+    Task<Tuple<bool, string>> CheckTokenSub(string tokenString, string userId);
 }
 
 public class HelperService : IHelperService
@@ -125,11 +125,18 @@ public class HelperService : IHelperService
         }
     }
 
-    public async Task<bool> CheckTokenSub(string tokenString, string userId)
+    public async Task<Tuple<bool, string>> CheckTokenSub(string tokenString, string userId)
     {
         var token = new JwtSecurityToken(jwtEncodedString: tokenString);
         string sub = token.Claims.First(c => c.Type == "sub").Value;
 
-        return sub.Equals(userId);
+        if (sub.Equals(userId))
+        {
+            return Tuple.Create(true, "");
+        }
+        else
+        {
+            return Tuple.Create(false, "Sub in the IdToken doesn't match user id in request body.");
+        }
     }
 }
