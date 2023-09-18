3790298d398fecf3b0a06eeac2b2cf664597837d
Caleb Archer <calebandcorey@gmail.com>
2023-09-18 18:06:44 +1200

    Remove lifecycle policy, as public ECR repos can't have them

--- infrastructure/container_registry.tf
+++ infrastructure/container_registry.tf
@@ -2,23 +2,3 @@ resource "aws_ecrpublic_repository" "web_app_image_repository" {
     provider = aws.us_east
     repository_name = "cinematica"
 }
-
-resource "aws_ecrpublic_repository_policy" "repository_lifecycle" {
-    repository_name = aws_ecrpublic_repository.web_app_image_repository.repository_name
-    provider = aws.us_east
-    
-    policy = jsonencode({
-        rules = [{
-            rulePriority = 1
-            description = "Delete older images"
-            selection = {
-                tagStatus = "any"
-                countType = "imageCountMoreThan"
-                countNumber = 2
-            }
-            action = {
-                type = "expire"
-            }
-        }]
-    })
-}
