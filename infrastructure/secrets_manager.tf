a8debfb8ae6ae6f22b348c47790d75fc46e02a15
Caleb Archer <calebandcorey@gmail.com>
2023-09-20 15:54:25 +1200

    Update secret recovery window (#107)
    * Set recovery window for secrets to 0
    
    * Create DB_PASSWORD_ARN env variable
    
    * Fix unsupported attribute for RDS instance

--- infrastructure/secrets_manager.tf
+++ infrastructure/secrets_manager.tf
@@ -1,7 +1,9 @@
 resource "aws_secretsmanager_secret" "tmdb_api_secret" {
     name = "TMDbApiKey"
+    recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret" "database_password" {
     name = "DB_PASSWORD"
+    recovery_window_in_days = 0
 }
