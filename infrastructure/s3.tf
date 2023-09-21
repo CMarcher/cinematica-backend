78c71a6665ceea4314e42674e1813eb716ce6f92
Caleb Archer <calebandcorey@gmail.com>
2023-09-21 22:18:05 +1200

    Protect S3 buckets and some other related resources from destruction

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -2,6 +2,10 @@
 
 resource "aws_s3_bucket" "api_lambda_bucket" {
     bucket = "cinematica-api-lambda-source"
+    
+    lifecycle {
+        prevent_destroy = true
+    }
 }
 
 resource "aws_s3_bucket_versioning" "api_lambda_bucket_versioning" {
@@ -9,6 +13,10 @@ resource "aws_s3_bucket_versioning" "api_lambda_bucket_versioning" {
     
     versioning_configuration {
         status = "Enabled"
+    }
+
+    lifecycle {
+        prevent_destroy = true
     }
 }
 
@@ -21,6 +29,10 @@ resource "aws_s3_bucket_notification" "api_lambda_events" {
 
 resource "aws_s3_bucket" "web_app_bucket" {
     bucket = "cinematica-front-end-source"
+
+    lifecycle {
+        prevent_destroy = true
+    }
 }
 
 resource "aws_s3_bucket_versioning" "web_app_bucket_versioning" {
@@ -28,6 +40,10 @@ resource "aws_s3_bucket_versioning" "web_app_bucket_versioning" {
 
     versioning_configuration {
         status = "Enabled"
+    }
+
+    lifecycle {
+        prevent_destroy = true
     }
 }
 
@@ -40,24 +56,40 @@ resource "aws_s3_bucket_notification" "web_app_events" {
 
 resource "aws_s3_bucket" "media_bucket" {
     bucket = "cinematica-media"
+
+    lifecycle {
+        prevent_destroy = true
+    }
 }
 
 resource "aws_s3_object" "movies_directory" {
     bucket = aws_s3_bucket.media_bucket.id
     key    = "movies/"
     content_type = "application/x-directory"
+
+    lifecycle {
+        prevent_destroy = true
+    }
 }
 
 resource "aws_s3_object" "posts_directory" {
     bucket = aws_s3_bucket.media_bucket.id
     key    = "posts/"
     content_type = "application/x-directory"
+
+    lifecycle {
+        prevent_destroy = true
+    }
 }
 
 resource "aws_s3_object" "users_directory" {
     bucket = aws_s3_bucket.media_bucket.id
     key    = "users/"
     content_type = "application/x-directory"
+
+    lifecycle {
+        prevent_destroy = true
+    }
 }
 
 resource "aws_s3_bucket_policy" "cinematica_media_policy" {
@@ -81,9 +113,17 @@ data "aws_iam_policy_document" "cinematica_media_policy_document" {
 
 resource "aws_s3_bucket" "api_pipeline_artifact_bucket" {
     bucket = "cinematica-api-codepipeline-artifact-store"
+
+    lifecycle {
+        prevent_destroy = true
+    }
 }
 
 # Front-end pipeline artifact bucket
 resource "aws_s3_bucket" "web_app_pipeline_artifact_bucket" {
     bucket = "cinematica-web-app-codepipeline-artifact-store"
+
+    lifecycle {
+        prevent_destroy = true
+    }
 }
