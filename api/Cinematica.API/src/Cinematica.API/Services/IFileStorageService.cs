c7e4641b6050e2bc9fb9307b5c8bf836abef1095
Bogun, Craig <cb349@students.waikato.ac.nz>
2023-09-25 20:53:01 +1300

    image location fixed for local

--- api/Cinematica.API/src/Cinematica.API/Services/IFileStorageService.cs
+++ api/Cinematica.API/src/Cinematica.API/Services/IFileStorageService.cs
@@ -10,6 +10,13 @@ public interface IFileStorageService
 
 public class LocalFileStorageService : IFileStorageService
 {
+    private ImageSettings _imageSettings;
+
+    public LocalFileStorageService(ImageSettings imageSettings)
+    {
+        _imageSettings = imageSettings;
+    }
+
     public async Task<string> SaveFileAsync(IFormFile file, string path)
     {
         // Generate a unique filename
@@ -17,6 +24,7 @@ public class LocalFileStorageService : IFileStorageService
 
         // Combine the savePath and the unique filename
         var fullPath = Path.Combine(path, fileName);
+        fullPath = Path.Combine(_imageSettings.UploadLocation, fullPath);
 
         await using var stream = new FileStream(fullPath, FileMode.Create);
         
