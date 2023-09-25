950ae7148b4c88d27eb22d7d2357377fffd09c67
David <d.nguyen7381@gmail.com>
2023-09-25 19:43:52 +1300

    updated users controller

--- api/Cinematica.API/src/Cinematica.API/Models/User/SetPictureRequest.cs
+++ api/Cinematica.API/src/Cinematica.API/Models/User/SetPictureRequest.cs
@@ -1,11 +1,12 @@
 namespace Cinematica.API.Models.User;
 
 using System.ComponentModel.DataAnnotations;
+using Cinematica.API.Models.Display;
 
 public class SetPictureRequest
 {
     [Required]
     public string UserId { get; set; }
     [Required]
-    public IFormFile File { get; set; }
+    public ImageModel Image { get; set; }
 }
